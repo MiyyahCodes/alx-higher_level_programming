ee6cae2243a58b534e25dfd0c0325ff9de9a7937
Adebimpe Islamiyyah Adefila <95176308+MiyyahCodes@users.noreply.github.com>
2022-06-27 21:23:10 +0100

    Update 101-nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,96 +1,92 @@
 #!/usr/bin/python3
-"""
-This module contains an algorithm that resolves the N-Queen puzzle
-using backtracking
-"""
+"""Module finds all solutions for N queens problem"""
 
 
-def isSafe(m_queen, nqueen):
-    """ Method that determines if the queens can or can't kill each other
+def solve(queens, size, start):
+    """Recursive function to solve N queens problem
     Args:
-        m_queen: array that has the queens positions
-        nqueen: queen number
-    Returns:
-        True: when queens can't kill each other
-        False: when some of the queens can kill
+        queens: number of queens to place
+        size: size of board
+    Returns True if valid move, False otherwise
     """
+    if queens == 0:
+        return True
+    if start[0] >= size or start[1] >= size:
+        return False
+    x = start[0]
+    y = start[1]
+    if board[x][y] == 0 and isValid(x, y, size):
+        board[x][y] = 1
+        if solve(queens - 1, size, (x + 1, 0)):
+            return True
+        board[x][y] = 0
+    return solve(queens, size, (x, y + 1))
 
-    for i in range(nqueen):
+#    for x in range(start[0], size):
+#        for y in range(start[1], size):
+#            if board[x][y] == 0 and isValid(x, y, size):
+#                board[x][y] = 1
+#                if solve(queens - 1, size, (x + 1, 0)):
+#                    return True
+#                board[x][y] = 0
+#    return False
 
-        if m_queen[i] == m_queen[nqueen]:
+
+def isValid(x, y, size):
+    """Returns whether a coordinate is valid and will not be attacked
+    Args:
+        x: x coordinate
+        y: y coordinate
+        size: size of board
+    Returns True if valid coordinate, False otherwise
+    """
+    for i in range(size):
+        if board[i][y] == 1 or board[x][i] == 1:
+            return False
+        dx = x + i
+        dy = y + i
+        if dx < size and dy < size and board[dx][dy] == 1:
+            return False
+        dx = x - i
+        dy = y - i
+        if dx > 0 and dy > 0 and board[dx][dy] == 1:
             return False
 
-        if abs(m_queen[i] - m_queen[nqueen]) == abs(i - nqueen):
-            return False
-
+        for j in range(size):
+            if i + j == x + y and board[i][j] == 1:
+                return False
+            if i - j == x - y and board[i][j] == 1:
+                return False
     return True
 
+if __name__ == "__main__":
+    from sys import argv
 
-def print_result(m_queen, nqueen):
-    """ Method that prints the list with the Queens positions
-    Args:
-        m_queen: array that has the queens positions
-        nqueen: queen number
-    """
-
-    res = []
-
-    for i in range(nqueen):
-        res.append([i, m_queen[i]])
-
-    print(res)
-
-
-def Queen(m_queen, nqueen):
-    """ Recursive function that executes the Backtracking algorithm
-    Args:
-        m_queen: array that has the queens positions
-        nqueen: queen number
-    """
-
-    if nqueen is len(m_queen):
-        print_result(m_queen, nqueen)
-        return
-
-    m_queen[nqueen] = -1
-
-    while((m_queen[nqueen] < len(m_queen) - 1)):
-
-        m_queen[nqueen] += 1
-
-        if isSafe(m_queen, nqueen) is True:
-
-            if nqueen is not len(m_queen):
-                Queen(m_queen, nqueen + 1)
-
-
-def solveNQueen(size):
-    """ Function that invokes the Backtracking algorithm
-    Args:
-        size: size of the chessboard
-    """
-
-    m_queen = [-1 for i in range(size)]
-
-    Queen(m_queen, 0)
-
-
-if __name__ == '__main__':
-
-    import sys
-
-    if len(sys.argv) == 1 or len(sys.argv) > 2:
+    if len(argv) != 2:
         print("Usage: nqueens N")
-        sys.exit(1)
+        exit(1)
 
     try:
-        size = int(sys.argv[1])
-    except:
+        n = int(argv[1])
+    except ValueError:
         print("N must be a number")
-        sys.exit(1)
+        exit(1)
 
-    if size < 4:
+    if n < 4:
         print("N must be at least 4")
-        sys.exit(1)
+        exit(1)
 
-    solveNQueen(size)
+    start = (0, 0)
+    while start[1] < n:
+        solutions = []
+        board = [[0 for i in range(n)] for j in range(n)]
+        if solve(n, n, start):
+            for x in range(n):
+                for y in range(n):
+                    if board[x][y] == 1:
+                        solutions.append([x, y])
+            print(solutions)
+            sy = solutions[0][1]
+        else:
+            sy = start[1]
+        start = (0, sy + 1)
